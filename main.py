4c9bad88631a5b2dd446aa8bfe114ef46c4cad45
Janielle Goolgar <Janielle.Goolgar@lexisnexisrisk.com>
2024-03-17 20:19:02 -0400

    added bullets

--- main.py
+++ main.py
@@ -12,10 +12,13 @@ BACKGROUND = pygame.transform.rotate(pygame.transform.scale(pygame.image.load(
                     os.path.join('Assets', 'space.png')), (config.HEIGHT,config.WIDTH)), 90
 )
 
-def draw_screen(player):
+def draw_screen(player,player_bullets):
     WIN.fill(config.WIN_COLOR)
     WIN.blit(BACKGROUND,(0,0))
     WIN.blit(SPACESHIP,(player.x,player.y))
+
+    for bullet in player_bullets:
+        pygame.draw.rect(WIN,config.BULLET_COLOR,bullet)
 
     pygame.display.update()
 
@@ -25,7 +28,14 @@ def player_movement(player, keys_pressed):
     if(keys_pressed[pygame.K_RIGHT] or keys_pressed[pygame.K_d]) and player.x + player.width + config.SPACESHIP_VEL < config.WIDTH:
         player.x += config.SPACESHIP_VEL
 
+def handle_bullets(player_bullets,player):
+    for bullet in player_bullets:
+        bullet.y -= config.BULLET_VEL
+        if bullet.y <= 0:
+            player_bullets.remove(bullet)
+
 def main():
+    pygame.display.set_caption("Space Invaders")
     clock = pygame.time.Clock()
     run = True
 
@@ -37,11 +47,16 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
+            if event.type == pygame.KEYDOWN:
+                if(event.key == pygame.K_UP or event.key == pygame.K_w or event.key == pygame.K_SPACE) and len(player_bullets) < config.MAX_BULLETS:
+                    bullet = pygame.Rect(player.x + player.width/2, player.y, config.BULLET_WIDTH, config.BULLET_HEIGHT)
+                    player_bullets.append(bullet)
         
 
         keys_pressed = pygame.key.get_pressed()
         player_movement(player, keys_pressed)
-        draw_screen(player)
+        draw_screen(player,player_bullets)
+        handle_bullets(player_bullets,player)
 
     
     pygame.quit()
